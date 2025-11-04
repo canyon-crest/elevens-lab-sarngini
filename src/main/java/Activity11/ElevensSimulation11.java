6dfd7b41c338d9d33b9f604cb25f307c094369d3
sarngini <sarngini@gmail.com>
2025-11-03 21:36:28 -0800

    recommit final

--- src/main/java/Activity11/ElevensSimulation11.java
+++ src/main/java/Activity11/ElevensSimulation11.java
@@ -8,7 +8,7 @@ public class ElevensSimulation11 {
 	/**
 	 * The number of games of Elevens to play.
 	 */
-	private static final int GAMES_TO_PLAY = 50;
+	private static final int GAMES_TO_PLAY = 10;
 
 	/**
 	 * Flag used to control debugging print statements.
